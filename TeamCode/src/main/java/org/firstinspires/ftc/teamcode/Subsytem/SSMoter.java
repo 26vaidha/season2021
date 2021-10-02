472780a56815c3298a34b08708c6bf128e039904
26vaidha <26vaidha@elmbrookstudents.org>
2021-10-02 16:06:33 -0500

    SSMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsytem/SSMoter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsytem/SSMoter.java
@@ -5,33 +5,82 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-public class SSMoter{
-
+public class SSMotor{
 
     Telemetry telemetry;
 
     //Object Declaration
-    DcMotor leftMoter;
+    DcMotor leftMotor;
+    DcMotor rightMotor;
 
     //Variable Declaration
-    //data_type variable_name = inital_value
-    static final int MOTER_TICK_COUNT = 1120;
+    //data_type variable_name = initial_value
+    static final int MOTOR_TICK_COUNT = 1120;
     double diameter = 5;
+    //double distance = 200;
+    int drivingTarget;
 
 
     double circumference = diameter* Math.PI;
 
-    public SSMoter(HardwareMap hardwareMap) {
+    public SSMotor(HardwareMap hardwareMap) {
 
         //connect software name to hardwarename
-        leftMoter = hardwareMap.dcMotor.get("left_motor");
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
     }
 
 
-    public void runLeftMoter(){
-        leftMoter.setMode (DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void resetMotor(){
 
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
+
+
+    public int calculateTargetPosition (double distance){
+
+        double circumference ;
+        double rotations;
+        double encoderDrivingTarget;
+
+        circumference = diameter * Math.PI;
+        rotations =  distance/ circumference;
+        encoderDrivingTarget =  5 * rotations * MOTOR_TICK_COUNT;
+
+        int drivingTarget = (int) encoderDrivingTarget;
+        return drivingTarget;
+    }//end of function
+
+    public void moveToTargetPosition(double distance){
+
+        int target = 0;
+        double distanceToBeMoved = 0;
+
+        distanceToBeMoved = calculateTargetPosition(distance);
+        target = calculateTargetPosition(distanceToBeMoved);
+
+
+        leftMotor.setTargetPosition(0);
+        rightMotor.setTargetPosition(0);
+
+        while (leftMotor.isBusy() || rightMotor.isBusy()){
+          telemetry.addLine ("Still driving");
+          telemetry.update();
+    }//end of function
+
+    public void stopMotors(){
+            leftMotor.setPower (0);
+            rightMotor.setPower (0);
+        }//end of function
+
+
+
+
+
+
+
+
 
 
 }//end of Class
