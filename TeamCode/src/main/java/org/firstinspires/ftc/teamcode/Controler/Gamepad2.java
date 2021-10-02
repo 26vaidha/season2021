39080f2e4cfeff88233e5da002fdbe1e7b8cbfff
26vaidha <26vaidha@elmbrookstudents.org>
2021-10-02 16:07:04 -0500

    Gamepad2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controler/Gamepad2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controler/Gamepad2.java
@@ -1,34 +1,9 @@
 package org.firstinspires.ftc.teamcode.Controler;
 
-import android.widget.Button;
-
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.;
 
-public class Gamepad2 {//start of class
-
-    Gamepad gamepad2;
-
-
-    //creating a constructor
-    public Gamepad2(Gamepad gamepad) (
-            gamepad2 = gamepad;
-    )
-
-    //Creating Button A (Function)
-    public ButtonA (Button A) (
-            ButtonA = gamepad;
-    )
-
-    public void gamepadAction() (
-            if ButtonA_selected(
-
-    )
-
-
-
-
-
-
+public class Gamepad2 {
+    public class
 }
